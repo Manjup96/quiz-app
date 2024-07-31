55c9a267ba44ccf769d9667e8679e467ced27c61
saikrushnakadarla <kadarlasaikrushna01@gmail.com>
2024-07-31 17:38:23 +0530

    Saikrushna

--- src/Admin/Components/Dashboard/Dashboard.js
+++ src/Admin/Components/Dashboard/Dashboard.js
@@ -21,33 +21,45 @@
 // export default Dashboard;
 
 
-
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuth } from '../../../Components/Context/AuthContext';
 import Header from '../../../Admin/Components/Header/Header';
-import '../../../Styles/Components/AdminDashboard.css'
-import Sidebar from '../../../Admin/Components/Sidebar/Sidebar'; // Adjust the import path according to your project structure
-//import '../../../Styles/Components/Dashboard.css'; // Add this line to import CSS for Dashboard component
+import '../../../Styles/Components/AdminDashboard.css';
+import Sidebar from '../../../Admin/Components/Sidebar/Sidebar';
+import { db } from '../../../Components/Firebase/FirebaseConfig'; // Adjust the import path according to your project structure
 
 const Dashboard = () => {
   const { user } = useAuth();
-  
+  const [userCount, setUserCount] = useState(0);
+
+  useEffect(() => {
+    const fetchUserCount = async () => {
+      try {
+        const usersSnapshot = await db.collection('users').get();
+        setUserCount(usersSnapshot.size);
+      } catch (error) {
+        console.error("Error fetching user count: ", error);
+      }
+    };
+
+    fetchUserCount();
+  }, []);
+
   console.log("User data:", user);
 
   return (
     <div>
       <Header />
-    <div className="dashboard-container">
-      
-      <Sidebar />
-      <div className="main-content">
-        <h1 className='admindashboard-heading'>Dashboard</h1>
-        <h2 className='admin-welcome'>Welcome Admin !!!</h2>
+      <div className="dashboard-container">
+        <Sidebar />
+        <div className="main-content">
+          <h1 className='admindashboard-heading'>Dashboard</h1>
+          <h2 className='admin-welcome'>Welcome Admin !!!</h2>
+          <p className='total-users'>Total Users: {userCount}</p> {/* Display user count here */}
+        </div>
       </div>
-    </div>
     </div>
   );
 };
 
 export default Dashboard;
-
