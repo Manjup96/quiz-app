d39938bdd0ec411d4b93429472cb59c77ee9a039
saikrushnakadarla <kadarlasaikrushna01@gmail.com>
2024-08-06 19:54:42 +0530

    Saikrushna

--- src/Admin/Components/Header/Header.js
+++ src/Admin/Components/Header/Header.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
 import company_logo from "../../../Admin/Components/Img/FoELogo_105x57 (1).png" ; 
-// import '../Styles/Header.css';
+import '../../../Styles/AdminHeader.css';
 import Logout from '../Logout/Logout';
 
 const Header = () => {
   return (
     <div>
-      <div className="header">
-      <img src={company_logo} alt="" className="company-logo"/>
+      <div className="headeradmin">
+      <img src={company_logo} alt="" className="company-logo-admin"/>
       <div className="logout-button">
         <Logout />
       </div>
