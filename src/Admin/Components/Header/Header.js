6b255a1051a9dc2038b48bf8cd80a19ddd914dda
saikrushnakadarla <kadarlasaikrushna01@gmail.com>
2024-08-07 14:31:53 +0530

    Saikrushna

--- src/Admin/Components/Header/Header.js
+++ src/Admin/Components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import company_logo from "../../../Admin/Components/Img/FoELogo_105x57 (1).png" ; 
+import chipper_sage_logo from "../Img/chipper-sage-logo.png";
 import '../../../Styles/AdminHeader.css';
 import Logout from '../Logout/Logout';
 
@@ -9,6 +10,8 @@ const Header = () => {
     <div>
       <div className="headeradmin">
       <img src={company_logo} alt="" className="company-logo-admin"/>
+      <h5><span className='flowhead-admin'>Flow Of English</span> - <span className='learnerhead-admin'>Learner</span></h5>
+      <img src={chipper_sage_logo} alt="" className="chipper-logo"/>
       <div className="logout-button">
         <Logout />
       </div>
